824f926a7f1f47241cdd2a4f6598c0002443cad9
Jun Liu <antyser@gmail.com>
2024-10-01 10:58:00 -0700

    fix

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -121,10 +121,10 @@ export default function Pricing() {
                 <ul className="text-gray-400 -mb-2 grow">
                   {plan.features.map((feature, featureIndex) => (
                     <li key={featureIndex} className="flex items-center mb-2">
-                      <svg className={`w-3 h-3 fill-current text-indigo-500 mr-3 shrink-0 ${featureIndex >= plan.includedFeatures && 'invisible'}`} viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <svg className="w-3 h-3 fill-current text-indigo-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span className={featureIndex >= plan.includedFeatures ? 'line-through' : ''}>{feature}</span>
+                      <span>{feature}</span>
                     </li>
                   ))}
                 </ul>
